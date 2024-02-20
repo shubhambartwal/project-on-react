52d8f0eba99d1f58fe72fe2cc29d47ac43ce8d0d
shubhambartwal <shubhambartwal58@gmail.com>
2024-02-20 20:23:39 +0530

    configure Appwrite

--- blogWithAppwrite/src/App.jsx
+++ blogWithAppwrite/src/App.jsx
@@ -2,7 +2,8 @@
 import './App.css'
 
 function App() {
-  console.log(import.meta.env.REACT_APP_APPWRITE_URL)
+  console.log(import.meta.env.VITE_APPWRITE_URL)
+  console.log("hi")
   return (
     <>
    <h1>sadfsaf</h1>
