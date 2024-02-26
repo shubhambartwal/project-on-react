9fa01a3eb8d25a05e09dd086b2936d847647eef0
shubhambartwal <shubhambartwal58@gmail.com>
2024-02-26 21:05:16 +0530

    created PostCard ,Select  component

---
+++ blogWithAppwrite/src/components/Login.jsx
@@ -0,0 +1,14 @@
+import React,{useState} from 'react'
+import { Link,useNavigate } from 'react-router-dom'
+import { login as authLogin } from '../store/authSlice'
+import {Button,Input,Logo} from './index'
+import { UseDispatch } from 'react-redux'
+import authService from '../appwrite/auth'
+import {useForm} from 'react-hook-form'
+function Login() {
+  return (
+    <div>Login</div>
+  )
+}
+
+export default Login
